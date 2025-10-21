bdd4a51170773c6bb3344c308c5ddd7362a9b9f3
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-10-21 18:56:36 +0530

    trying with standalone builds

--- apps/webcase/next.config.ts
+++ apps/webcase/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
