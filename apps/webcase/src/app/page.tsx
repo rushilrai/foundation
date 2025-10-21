57015d20f19e3ad7ad14bb80bcd635a627f5b854
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-10-21 16:35:34 +0530

    shadcn set up and theming

--- apps/webcase/src/app/page.tsx
+++ apps/webcase/src/app/page.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Button>Click me</Button>
+        <Button variant="destructive" onClick={() => window.alert("clicked")}>Click me</Button>
       </main>
     </div>
   );
