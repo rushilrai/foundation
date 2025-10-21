b1b1f9ddc958c17b6015184e6d8684dcfae4a7c3
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-10-21 19:04:23 +0530

    deploying again

--- apps/webcase/src/app/page.tsx
+++ apps/webcase/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Button variant="destructive" onClick={() => window.alert("clicked")}>TEST BUTTON</Button>
+        <Button variant="destructive" onClick={() => window.alert("clicked")}>TEST</Button>
       </main>
     </div>
   );
