6464a6ce6f20788d0afc1865ae1924d93f23464f
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-10-24 16:40:38 +0530

    upgraded to nextjs 16

--- apps/webcase/next.config.ts
+++ apps/webcase/next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
 
 export default nextConfig;
