f0ddd4ad160883d6a98d340fbd6d7fd1d709fdb9
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2026-01-26 01:13:00 +0530

    initial theme setup for webcase (not final)

--- apps/webcase/src/routes/home.tsx
+++ apps/webcase/src/routes/home.tsx
@@ -1,24 +1,27 @@
+import { SignOutButton } from '@clerk/tanstack-react-start'
 import { auth } from '@clerk/tanstack-react-start/server'
 import { createFileRoute, redirect } from '@tanstack/react-router'
 import { createServerFn } from '@tanstack/react-start'
 
 const authGuard = createServerFn({ method: 'GET' }).handler(async () => {
-    const { userId } = await auth()
-    if (!userId) {
-        throw redirect({ to: '/sign-in/$' })
-    }
-    return { userId }
+  const { userId } = await auth()
+  if (!userId) {
+    throw redirect({ to: '/sign-in/$' })
+  }
+  return { userId }
 })
 
 export const Route = createFileRoute('/home')({
-    beforeLoad: () => authGuard(),
-    component: HomePage,
+  beforeLoad: () => authGuard(),
+  component: HomePage,
 })
 
 function HomePage() {
-    return (
-        <div>
-            <h1>Welcome to your dashboard</h1>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome to your dashboard</h1>
+
+      <SignOutButton />
+    </div>
+  )
 }
--- apps/webcase/src/routes/index.tsx
+++ apps/webcase/src/routes/index.tsx
@@ -1,4 +1,6 @@
+import { Button } from '@/components/ui/button'
 import { SignedIn, SignedOut } from '@clerk/tanstack-react-start'
+import { IconArrowUpRight } from '@tabler/icons-react'
 import { createFileRoute, Link, Navigate } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({
@@ -7,18 +9,39 @@ export const Route = createFileRoute('/')({
 
 function IndexRouteComponent() {
   return (
-    <>
+    <div className="h-full w-full flex flex-col justify-center items-center">
       <SignedOut>
-        <div>
-          <h1>Welcome to WebCase</h1>
-          <p>Your app description here</p>
-          <Link to="/sign-in/$">Sign In</Link>
+        <div className="flex flex-row gap-12 items-center">
+          <div className="flex flex-col">
+            <h1 className="text-4xl font-semibold text-primary">WebCase.</h1>
+
+            <p className="mt-1 text-muted-foreground max-w-sm">
+              Save articles, videos, and links from anywhere.
+              <br />
+              Read them later, distraction-free.
+            </p>
+
+            <Button
+              className="mt-4 max-w-36"
+              render={
+                <Link to="/sign-in/$">
+                  Get Started <IconArrowUpRight />
+                </Link>
+              }
+            />
+          </div>
+
+          <img
+            src="/images/webcase-graphic.png"
+            alt="Webcase Graphic"
+            className="w-64"
+          />
         </div>
       </SignedOut>
 
       <SignedIn>
         <Navigate to="/home" />
       </SignedIn>
-    </>
+    </div>
   )
 }
